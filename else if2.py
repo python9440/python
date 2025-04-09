d825ec18ad059add477ba036fe0dc52461ae7e23
python9440 <kummethabharathi93@gmail.com>
2025-04-08 17:34:05 -1200

    Add files via upload

---
+++ else if2.py
@@ -0,0 +1,6 @@
+num1=int(input("enter first nmber:"))
+num2=int(input("enter second number:"))
+if num1>=num2:
+    print("the given number is bigger number")
+else:
+    print("the given number is smaller number")
