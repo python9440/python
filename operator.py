c700a74b887356cdece39fb18bad1aa520efe41c
python9440 <kummethabharathi93@gmail.com>
2025-04-08 17:31:01 -1200

    Add files via upload

---
+++ operator.py
@@ -0,0 +1,6 @@
+name="python"
+sal=50000
+cf="java"
+print("hello{} your friend{} is waiting and your sal is{}".format(name,cf,sal))
+print("hello{0} your friend{2} is waiting and your sal is{1}".format(name,sal,cf))
+print("hello{x} your friend{y} is waiting and your sal is{z}".format(y=cf,z=sal,x=name))
