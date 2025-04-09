06dd60ce14750e20fc8d6eafe5a4fd390979384a
python9440 <kummethabharathi93@gmail.com>
2025-04-08 17:33:23 -1200

    Add files via upload

---
+++ while loop5.py
@@ -0,0 +1,12 @@
+n=int(input("enter a number:"))
+dup=n
+sum=0
+while n>0:
+    rem=n%10
+    sum=sum+(rem**3)
+    n=n//10
+    if dup==sum:
+        print("the given number is armstrong")
+else:
+    print("the given number is not armstrong")
+    
