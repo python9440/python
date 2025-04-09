dd106c61c58c55a5810aed2e206154d7f09d499d
python9440 <kummethabharathi93@gmail.com>
2025-04-08 17:27:12 -1200

    Add files via upload

---
+++ if elif.py
@@ -0,0 +1,9 @@
+num1=int(input("enter first number"))
+num2=int(input("enter second number"))
+num3=int(input("enter third number"))
+if num1>=num2 and num1>num3:
+    print("num2 is greater")
+elif num2>num3:
+    print("num2 is greater")
+else:
+    print("the given number is smaller")
