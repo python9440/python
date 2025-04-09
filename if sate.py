1d48c1f0e5e4e78fa952a17c3e9cab9a77813d41
python9440 <kummethabharathi93@gmail.com>
2025-04-08 17:28:01 -1200

    Add files via upload

---
+++ if sate.py
@@ -0,0 +1,12 @@
+n1=int(input("enter first number:"))
+n2=int(input("enter second number:"))
+n3=int(input("enter third number:"))
+n4=int(input("enter fourth number:"))
+s=n1
+if n2<s:
+    s=n2
+if n3<s:
+    s=n3
+if n4<s:
+    s=n4
+    print("the smaller number is:",s)
