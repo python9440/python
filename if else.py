fcb5ab7b320595f504fa511fed8782f60da41f74
python9440 <kummethabharathi93@gmail.com>
2025-04-08 17:27:34 -1200

    Add files via upload

---
+++ if else.py
@@ -0,0 +1,11 @@
+name=input("enter your choice:")
+if name=="strawberry":
+    print("it is children flavour")
+elif name=="chocolate":
+    print("it is too much tasty")
+elif name=="venella":
+    print("buy one getvone free")
+elif name=="blackcurrent":
+    print("it is very tasty")
+else:
+    print("other flavours are not recommended:")
